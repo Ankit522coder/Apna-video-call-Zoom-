db1cc8afc033a114c84396cef4f954f396c74378
Ankit522coder <sagarnain18131421@gmail.com>
2026-07-14 13:11:45 +0530

    Update Project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import { AuthProvider } from './contexts/AuthContext';
 import VideoMeetComponent from './pages/VideoMeet';
 import HomeComponent from './pages/home';
 import History from './pages/history';
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return <Navigate to='/auth' replace />;
+  }
+
+  return children;
+};
 
 function App() {
   return (
@@ -22,9 +32,30 @@ function App() {
 
             <Route path='/auth' element={<Authentication />} />
 
-            <Route path='/home' element={<HomeComponent />} />
-            <Route path='/history' element={<History />} />
-            <Route path='/:url' element={<VideoMeetComponent />} />
+            <Route
+              path='/home'
+              element={
+                <ProtectedRoute>
+                  <HomeComponent />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/history'
+              element={
+                <ProtectedRoute>
+                  <History />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/:url'
+              element={
+                <ProtectedRoute>
+                  <VideoMeetComponent />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </AuthProvider>
 
